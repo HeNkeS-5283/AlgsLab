2900ceb0c2db8631717d6a911db5959c79bf2cdc
Sasha <sasha2003.034@gmail.com>
2024-02-20 11:49:12 +0200

    Lab1

--- Lab1/bu-algs-01-lists-master/src/main/java/com/bu/algs/impl/BuArrayList.java
+++ Lab1/bu-algs-01-lists-master/src/main/java/com/bu/algs/impl/BuArrayList.java
@@ -1,38 +1,58 @@
 package com.bu.algs.impl;
 
 import com.bu.algs.BuList;
-
-import java.util.ArrayList;
 import java.util.Comparator;
 
 // TODO: implement as dynamic array. Do not use Java collections of any utils.
 public class BuArrayList<T> implements BuList<T> {
-
-    private ArrayList<T> arrayList = new ArrayList<T>();
-
+        Object[] arrayList = new Object[0];
     @Override
     public int size() {
-        return arrayList.size();
+        return arrayList.length;
     }
 
     @Override
     public boolean isEmpty() {
-        return arrayList.isEmpty();
+        return arrayList.length == 0;
     }
 
     @Override
     public boolean contains(T elem) {
-        return arrayList.contains(elem);
+        for (int i = 0; i < arrayList.length; i++) {
+            if (arrayList[i] == elem) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
     public void clear() {
-       arrayList.clear();
+        arrayList = new Object[0];
     }
 
     @Override
     public boolean add(T elem) {
-        return arrayList.add(elem);
+        if(arrayList.length > 0) {
+            Object[] cloneArray = new Object[arrayList.length];
+
+            for (int i = 0; i < arrayList.length; i++) {
+                cloneArray[i] = arrayList[i];
+            }
+
+            arrayList = new Object[arrayList.length+1];
+
+            for (int i = 0; i < cloneArray.length; i++) {
+                arrayList[i] = cloneArray[i];
+            }
+            arrayList[arrayList.length-1] = elem;
+            return true;
+        }
+            else {
+            arrayList = new Object[arrayList.length+1];
+            arrayList[0] = elem;
+            return true;
+        }
     }
 
     @Override
@@ -42,20 +62,42 @@ public class BuArrayList<T> implements BuList<T> {
 
     @Override
     public T get(int index) {
-        return arrayList.get(index);
+        return null;
     }
 
     @Override
     public boolean remove(T elem) {
-        return arrayList.remove(elem);
+        int n = -1;
+        for (int i = 0; i < arrayList.length; i++) {
+            if (arrayList[i] == elem) {
+                n = i;
+                break;
+            }
+        }
+        Object[] cloneArray = new Object[arrayList.length-1];
+        for (int i = 0; i < cloneArray.length; i++) {
+            if (i != n) {
+                cloneArray[i] = arrayList[i];
+            }
+        }
+        arrayList = cloneArray;
+        return true;
     }
 
     @Override
     public T remove(int index) {
-        return arrayList.remove(index);
+        Object[] cloneArray = new Object[arrayList.length-1];
+        for (int i = 0; i < cloneArray.length; i++) {
+            if (i != index) {
+                cloneArray[i] = arrayList[i];
+            }
+        }
+        arrayList = cloneArray;
+        return (T) arrayList;
     }
 
     @Override
     public void sort(Comparator<T> comparator) {
-        arrayList.sort(comparator);}
+
+    }
 }
